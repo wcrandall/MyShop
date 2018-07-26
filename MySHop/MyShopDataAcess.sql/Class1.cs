d6a4854eeead9f5c4e87cfd082503b4ebef341c8
wcrandall <aweserific@gmail.com>
2018-07-25 19:22:37 -0500

    dude I did lots of things

---
+++ MySHop/MyShopDataAcess.sql/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopDataAcess.sql
+{
+    public class Class1
+    {
+    }
+}
